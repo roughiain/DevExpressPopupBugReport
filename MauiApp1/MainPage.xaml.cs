a8f74240607b9f673e7457c49860a10e6e190d5f
Iain Rough <roughiain@gmail.com>
2023-10-23 16:42:56 +0200

    Initial Commit

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        System.Diagnostics.Debug.WriteLine($"PopUp.IsOpen : {Popup.IsOpen}");
+        DebugLabel.Text = $"PopUp.IsOpen : {Popup.IsOpen}";
+    }
+}
